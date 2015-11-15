aa16285af5f0c499d075db0a5ed5bc69190fbaff
Brian Jo <brian88777@gmail.com>
2015-11-14 22:48:34 -0500

    Final fix

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-    var myDataRef = new Firebase('https://popping-heat-9713.firebaseio.com/');
+    var myDataRef = new Firebase('https://fiery-torch-7339.firebaseio.com/');
     var textDict = {}
 
     // $('#annotation').keypress(function (e) {
@@ -22,20 +22,25 @@
              var tempList = []
              for (var word in res) 
              {
-                console.log(res[word])
                 found = false
                 for (var key in textDict) {
                   tempList = textDict[key]
-                  console.log(tempList)
                   for (var value in tempList) {
                     //console.log(tempList[value])
                     if (res[word].toLowerCase() === tempList[value] && !found)
                     { 
                       console.log(res[word] === tempList[value])
                       translate.push('<img class="emoji" draggable="false" src="images/' + key + '_apple.png">') //append key
-                      translate_alt.push(key) //append key
-                      found = true
+                      
                       intList = tempList[0]
+                      integer_List = intList.split('_')
+                          if (integer_List.length === 1) {
+                            translate_alt.push(String.fromCharCode(parseInt(integer_List[0])));
+                         } else {
+                            translate_alt.push(String.fromCharCode(parseInt(integer_List[0]),parseInt(integer_List[1])));
+                          }
+                      
+                      found = true
                       console.log(intList)
                       break
                     }
@@ -50,31 +55,21 @@
           console.log(translate)
           var toSend = String(translate.join(" "));
           document.getElementById('result').innerHTML = toSend;
-          return intList
+          console.log(translate_alt)
+          return translate_alt
         }
 
   function myFacebookLogin() {
 FB.login(function(){
  // Note: The call will only work if you accept the permission request
  intList = printCaps();
- integer_List = intList.split('_')
- if (integer_List.length === 1) {
-    FB.api('/me/feed', 'post', {message: String.fromCharCode(parseInt(integer_List[0]))});
- } else {
-    FB.api('/me/feed', 'post', {message: String.fromCharCode(parseInt(integer_List[0]),parseInt(integer_List[1]))});
-  }
+  FB.api('/me/feed', 'post', {message: String(intList.join(" "))});
 }, {scope: 'publish_actions'});} 
-
 
 
 function temp_function() {
   intList = printCaps();
-  integer_List = intList.split('_')
-  if (intList.length === 1) {
-    alert(String.fromCharCode(parseInt(intList[0])));
- } else {
-    alert(String.fromCharCode(parseInt(intList[0]),parseInt(intList[1])));
-  }
+  alert(String(intList.join(" ")));
 }
 
 // function getRandomImage() {

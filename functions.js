7628757a573a21024f031e416e663fb004ad1e1b
Brian Jo <brian88777@gmail.com>
2015-11-14 21:59:25 -0500

    emoji fix test

--- functions.js
+++ functions.js
@@ -18,32 +18,61 @@
              var textstr = document.myform.text.value;
              var res = textstr.split(" ");
              var translate = []
+             var translate_alt = []
              var tempList = []
+             var intList = []
              for (var word in res) 
              {
                 console.log(res[word])
                 found = false
                 for (var key in textDict) {
-                  tempList = textDict[key]
+                  tempList = textDict[key]['anno']
                   //console.log(tempList)
                   for (var value in tempList) {
                     //console.log(tempList[value])
-                    if (res[word].toLowerCase(); === tempList[value] && !found)
+                    if (res[word].toLowerCase() === tempList[value] && !found)
                     { 
                       console.log(res[word] === tempList[value])
                       translate.push('<img class="emoji" draggable="false" src="images/' + key + '_apple.png">') //append key
+                      unicode_emoji = String.fromCharCode(key)
+                      translate_alt.push(unicode_emoji) //append key
                       found = true
                       break
                     }
                   }
+                  intList = textDict[key]['dec']
                 }
                 if (!found)
                   {
                     translate.push(res[word])
+                    translate_alt.push(res[word])
                   }
            }
           console.log(translate)
           var toSend = String(translate.join(" "));
           document.getElementById('result').innerHTML = toSend;
+          return intList
         }
+
+  function myFacebookLogin() {
+FB.login(function(){
+ // Note: The call will only work if you accept the permission request
+ intList = printCaps();
+ if intList.length === 1 {
+    FB.api('/me/feed', 'post', {message: String.fromCharCode(parseInt(intList[0]))});
+ } else {
+    FB.api('/me/feed', 'post', {message: String.fromCharCode(parseInt(intList[0]),parseInt(intList[1]))});
+  }
+}, {scope: 'publish_actions'});} 
+
+
+
+function temp_function() {
+  intList = printCaps();
+ if intList.length === 1 {
+    alert(String.fromCharCode(parseInt(intList[0])));
+ } else {
+    alert(String.fromCharCode(parseInt(intList[0]),parseInt(intList[1])));
+  }
+}
 
---
+++ scrape_surrogate.py
@@ -0,0 +1,48 @@
+
+import csv
+import re
+import json
+import urllib2
+
+input_file = open('/Users/brian_jo/Desktop/Playground/text_to_emoji/dataset.csv')
+
+input_url = 'http://www.fileformat.info/info/unicode/char/'
+suffix = '/index.htm'
+
+input_file.readline()
+
+worddic = {}
+
+i = 0
+for line in input_file.readlines():
+        html_id = str.split(line.strip(), ',')[0]
+        anno = str.split(line.strip(), ',')[-1]
+        anno_array = str.split(anno, ';')
+
+        search_url = input_url + html_id + suffix
+
+        try:
+                conn = urllib2.urlopen(search_url)
+                text = conn.read()
+                ind = text.find('UTF-16 (decimal)')
+                new_text = text[ind+43:]
+                ind2 = new_text.find(' <')
+                surr_code = new_text[:ind2]
+                surr_code_int = str.replace(surr_code,',','')
+                arr = str.split(surr_code_int," ")
+        except:
+                continue
+
+        i = i+1
+        if i%100 == 0:
+                print str(i)
+
+        if html_id not in worddic:
+                worddic[html_id] = {}
+
+        worddic[html_id]['dec'] = arr
+        worddic[html_id]['anno'] = anno_array
+
+with open('/Users/brian_jo/Desktop/Playground/text_to_emoji/jsondata.txt', 'w') as outfile:
+        json.dump(worddic, outfile)
+
